6b484ffa0f33489d4d749ae87b39f184e68f1d98
gtech <rithwanul.islam@gtechcp.com>
2021-12-15 18:51:22 +0600

    Configuring navigation graph

--- app/src/main/java/com/example/roomdatabase/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/ui/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.roomdatabase;
+package com.example.roomdatabase.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.roomdatabase.R;
 
 public class MainActivity extends AppCompatActivity {
 
